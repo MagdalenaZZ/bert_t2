fd9d55334c5687c8e24c213b8520aefdeebf2ae1
MagdalenaZZ <mzarowiecki@gmail.com>
2023-02-14 19:00:51 +0000

    Update modeling.py
    try again

--- modeling.py
+++ modeling.py
@@ -360,13 +360,12 @@ def dropout(input_tensor, dropout_prob):
 
 '''
 #def layer_norm(input_tensor, name=None):
-#  """Run layer normalization on the last dimension of the tensor."""
 #  return tf.contrib.layers.layer_norm(
 #      inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
 '''
 
 def layer_norm(input_tensor, name=None):  # new
-  """Run layer normalization on the last dimension of the tensor."""  # new
+  """Run layer normalization on the last dimension of the tensor.""" 
   layer_norma = tf.keras.layers.LayerNormalization(axis = -1)  # new
   return layer_norma(input_tensor)  # new
 
